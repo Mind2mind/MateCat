4b7ae214280c61e687ae32b85ae7388e337dbc30
Fabrizio Regini <freegenie@gmail.com>
2016-11-04 00:37:52 +0100

    version working with one term

--- test.js
+++ test.js
@@ -36,7 +36,36 @@ var Transformer = function( element ) {
         $(node).replaceWith( newText );
     }
 
-    var removeParentAndPreserveCaret = function() {
+
+    var transformNodeAndPreserveCursor = function(node, term) {
+        var newNode = $('<span/>').addClass(this.selector) ;
+        newNode.text( term );
+
+        var offset ;
+        var range = window.getSelection().getRangeAt(0);
+        if ( range.endContainer == node ) {
+            offset = range.endOffset ;
+        }
+
+        $(node).replaceWith( newNode );
+
+
+        if ( offset ) {
+
+            // TODO: duplicated code
+            var r = document.createRange();
+            r.setStart(newNode[0].childNodes[0], offset);
+            r.setEnd(newNode[0].childNodes[0], offset);
+
+            var sel = window.getSelection();
+
+            sel.removeAllRanges();
+            sel.addRange( r );
+        }
+
+    };
+
+    var removeParentAndPreserveCursor = function() {
         var r = window.getSelection().getRangeAt(0);
         var offset = r.endOffset ;
 
@@ -48,16 +77,17 @@ var Transformer = function( element ) {
         }
 
         var newNode = document.createTextNode(r.endContainer.textContent);
-        var replaced = parent.replaceWith( newNode );
-        var range = document.createRange();
+        parent.replaceWith( newNode );
 
+        // TODO: duplicated code
+        var range = document.createRange();
         range.setStart(newNode, offset);
         range.setEnd(newNode, offset);
-
         var sel = window.getSelection();
-
         sel.removeAllRanges();
         sel.addRange(range);
+
+        return newNode ;
     }
 
     var startLooping = function() { 
@@ -74,47 +104,71 @@ var Transformer = function( element ) {
         descend.call( this, this.element, this.term ); 
     };
 
-    var isolateCaret = function( node, term ) { 
+    var isolateCursor = function( node, term ) {
         // first off, see if the node is already wrapper and remove the parent
-        removeParentAndPreserveCaret.call(this);
+        var replaced = removeParentAndPreserveCursor.call(this);
+
+        if ( replaced ) {
+            node = replaced ;
+        }
 
         // then split the text we want to replace stands in its own text node
         var escaped = escapeRegExp( term );
         var expForSplit = new RegExp('\\b' + escaped + '\\b',"g");
         var baseNode = node.cloneNode() ;
-        var matches ; 
+        var matches =  [];
         var rightPart ; 
         var leftPart ; 
 
         var chunks = []; 
+
+        var that = this ;
         while ((match = expForSplit.exec( node.textContent )) !== null) {
-            if ( match.index === 0 ) { 
+            matches.push( match );
+        }
+
+        // iterate matches inversely to isolate nodes
+        for ( var i = matches.length -1 ; i > -1 ; i--) {
+            var match = matches[i] ;
+
+
+            if ( node.textContent == term ) {
+                chunks.push( node );
                 continue ; 
             }
 
-            var rightPart = node.splitText( match.index );
-            var leftPart = rightPart.previousSibling ;
+            rightPart = node.splitText( match.index + term.length );
+            if ( rightPart ) chunks.push( rightPart );
 
-            chunks.push( leftPart ); 
+            rightPart = node.splitText( match.index );
+            if ( rightPart ) chunks.push( rightPart );
 
-            node = baseNode.cloneNode(); 
+            leftPart = node ;
+
         }
 
-        chunks.push( rightPart ); 
+        if ( leftPart ) chunks.push( leftPart );
 
+        // for each chunk we make a loop and
+        chunks.reverse();
+
+        $.each(chunks, function(index, node) {
+            if ( node.textContent == term ) {
+                transformNodeAndPreserveCursor.call(that, node, term);
+            }
+        });
         console.log( chunks ); 
 
         // ok now we splitted nodes 
     };
 
     var analyze = function(node, term) { 
-
         // is the current node matching the one with caret?
 
         if ( this.currentRange && node === this.currentRange.endContainer ) {
-            isolateCaret.call( this, node, term ) ; 
+            isolateCursor.call( this, node, term ) ;
         } else { 
-            searchAndReplaceWord.call(this, node, term ); 
+            searchAndReplaceWord.call( this, node, term );
         }
 
     }
